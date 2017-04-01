c0e5c4a8e68584f9d3a98ef794c94f8929fe41e0
Silvano <silvo34@live.nl>
2017-04-01 18:57:15 +0200

    added stuff

--- src/Ona.js
+++ src/Ona.js
@@ -1,11 +1,13 @@
 global.Promise = require('bluebird');
 
+const path = require('path');
+
 const config = require('../config');
 const SilvanoClient = require('./structures/SilvanoClient.js');
 
 const client = new SilvanoClient({
 	prefix: config.prefix,
-	commandsDir: config.commands,
+	commandsDir: path.isAbsolute(config.commands) ? config.commands : path.join(__dirname, config.commands),
 });
 
 client.login(config.token);
